2712ef2c2fff091953c2e8453f6e5143b4489b5f
lucasdamasceno96 <ldp21k@outlook.com>
2025-06-21 14:55:17 -0300

    feat\ starting validation with go-playground

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,11 +1,19 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/lucasdamasceno96/go-crud-api-mvc/src/config/rest_err"
+	"github.com/lucasdamasceno96/go-crud-api-mvc/src/model/request"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestError("Wrong method !")
-	c.JSON(err.Code, err)
+	var userRequest request.UserRequest
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(fmt.Sprintf("There some incorrect fields, error=%s", err.Error()))
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+	fmt.Println(userRequest)
 }
---
+++ src/model/request/user_request.go
@@ -0,0 +1,8 @@
+package request
+
+type UserRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=8,max=12,containsany=!@#$%&*+-"`
+	Name     string `json:"name" binding:"required,min=4,max=50"`
+	Age      int8   `json:"age" binding:"required,numeric,min=1,max=3"`
+}
